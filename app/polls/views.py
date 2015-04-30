addd39a42aefbac857e8c059e7171df255a25287
Ferdi Ramdhon <ferdirn@gmail.com>
2015-04-30 14:15:17 +0700

    update detail in polls/views

--- app/polls/views.py
+++ app/polls/views.py
@@ -1,14 +1,21 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import Http404, HttpResponse
+from .models import Question
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello world! You're at the polls index.")
+    latest_questions = Question.objects.order_by('-pub_date')[:5]
+    # output = ", ".join([p.question_text for p in latest_questions])
+    context = {'latest_questions': latest_questions, 'name': "Ferdi Ramdhon"}
+    return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    response = "You're looking at question %s." % question_id
-    return HttpResponse(response)
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question with id {} does not exist.".format(question_id))
+    return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
